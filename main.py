7526f785afcbc84f1de17415255345fcf8388515
SnehalChodankar <snehalchodankar95@gmail.com>
2024-04-14 16:16:39 +0530

    final changes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -9,12 +10,72 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    global reps
+    global timer
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    label_title.config(text="Timer", fg=GREEN)
+    ticket_label.config(text="")
+    reps = 0
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    global reps
+    global label_title
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    reps += 1
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        label_title.config(text="Long Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        label_title.config(text="Short Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        label_title.config(text="Work", fg=GREEN)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    # print(count)
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    if count_min < 10:
+        count_min = f"0{count_min}"
+
+    # print(mins, sec)
+
+    timeform = str(count_min) + ":" + str(count_sec)
+
+    canvas.itemconfig(timer_text, text=timeform)
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        mark = ""
+        work_session = math.floor(reps / 2)
+        for _ in range(work_session):
+            mark += "✔"
+        ticket_label.config(text=mark)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -24,20 +85,19 @@ window.config(padx=100, pady=50, bg=YELLOW)
 label_title = Label(text="Timer", font=(FONT_NAME, 35, "bold"), bg=YELLOW, fg=GREEN)
 label_title.grid(column=2, row=1)
 
-canvas = Canvas(width=240, height=224 , bg=YELLOW, highlightthickness=0)
+canvas = Canvas(width=240, height=224, bg=YELLOW, highlightthickness=0)
 img = PhotoImage(file="./tomato.png")
-canvas.create_image(120,112, image=img)
-canvas.create_text(120,130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.create_image(120, 112, image=img)
+timer_text = canvas.create_text(120, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=2, row=2)
 
-start_btn = Button(text="Start")
+start_btn = Button(text="Start", command=start_timer)
 start_btn.grid(column=1, row=3)
 
-reset_btn = Button(text="Reset")
+reset_btn = Button(text="Reset", command=reset_timer)
 reset_btn.grid(column=3, row=3)
 
-ticket_label = Label(text="✔", bg=YELLOW, fg=GREEN)
+ticket_label = Label(bg=YELLOW, fg=GREEN)
 ticket_label.grid(column=2, row=4)
-
 
 window.mainloop()
